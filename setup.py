31b490128a260299327c119c21507eb9b65210b2
Connor Nickol <cnickol26@gmail.com>
2022-07-11 11:39:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='M14_homework',
+setup(name='M14_hk',
       version='0.1',
       description='A Homework package from DS5100',
       url='https://github.com/cnickol26/M14_homework',
       author='Connor Nickol',
       author_email='can2hr@virginia.edu',
       license='MIT',
-      packages=['M14_homework'])
+      packages=['M14_hk'])
